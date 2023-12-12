c4b908d0558cfa46dc1951ff75c3a0158614dade
kinsiabdirahman <kinsiabdirahman@outlook.com>
2023-12-12 23:58:19 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// question 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// question 2
+let random = Math.floor(Math.random() * 100) + 1;
+
+// question 3
+
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4;
+
+// question 4
+let max = Math.max(20);
